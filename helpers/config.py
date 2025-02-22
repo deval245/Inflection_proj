aa3c91834a995fc9dbaf6bb80f23fa0262568e7b
deval.thakkar <deval.praveenchandra.thakkar.-nd@disney.com>
2025-02-22 15:24:31 +0530

    configfile added

---
+++ helpers/config.py
@@ -0,0 +1,7 @@
+BASE_URL_TEMPLATE = "http://localhost:7071"
+BASE_URL_RECIPIENT = "http://localhost:7072"
+BASE_URL_CAMPAIGN = "http://localhost:7070"
+
+HEADERS = {"accept": "*/*"}
+
+TIMEOUT = 10  # Define timeout for API requests
